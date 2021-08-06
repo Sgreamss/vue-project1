8ad37bf258aeac10fc8331c9bdd0d0bdb3dce9e9
Sgreamss <earthwitchapol2@gmail.com>
2021-08-06 17:46:02 +0700

    add axios

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
